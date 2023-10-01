5bc26002ccb80c81ba4b7e298602db4df9860988
DarthSigil <servantofchristvigil@mail.com>
2023-09-30 20:13:58 -0800

    initial blank react app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import '../css/app.css'
+
+function App() {
+
+  return (
+    <div className="app">Here is my App!</div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/App.js'
+
+import './css/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App/>
+);
