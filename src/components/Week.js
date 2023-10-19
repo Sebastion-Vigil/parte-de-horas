62fb69fcc171977c8b40deb42eb708052a7c563f
DarthSigil <servantofchristvigil@mail.com>
2023-10-18 17:11:02 -0800

    week rows/cols looking nice, also figured out true reason for previous react invalid child error: was trying to access state arr of vals and didnt realize that I needed to use descriptions[0]

--- src/components/Week.js
+++ src/components/Week.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 import WeekDescriptionRow from './WeekDescriptionRow.js';
+import DaySum from './DaySum.js';
 
 const Week = () => {
     return (
         <div className='week'>
             <WeekDescriptionRow />
+            <DaySum />
+            <DaySum />
+            <DaySum />
+            <DaySum />
+            <DaySum />
+            <DaySum />
+            <DaySum />
         </div>
     )
 }
--- src/components/WeekDescriptionRow.js
+++ src/components/WeekDescriptionRow.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 
 const WeekDescriptionRow = () => {
-    const descriptions = useState(['Date', 'Start Time', 'Break Start', 'Break End', 'End Time', 'Total/Reg/OT'])
+    const descriptions = useState(['Date', 'Start Time', 'Break Start', 'Break End', 'End Time', 'Total / Reg / OT'])
     return (
-        <div className='description-row'>
+        <div className='desc-row flx-rw-spc-btw-cnt'>
             {
-                descriptions.map((v, i) => {
+                descriptions[0].map((v, i) => {
                     return (
-                        <div key={i} className='description'>v</div>
+                        <div key={i} className='desc-cell flx-cl-cnt-cnt'>{v}</div>
                     )
                 })
             }
@@ -18,3 +18,5 @@ const WeekDescriptionRow = () => {
 export default WeekDescriptionRow;
 // the v in {} threw f()s not valid as react child err
 // <div key={i} className='description'>{v}</div>
+// descriptions is an obj w/len 2; 0th item is arr of vals
+// now w/[0] in front of it the {v} doesn't throw err
