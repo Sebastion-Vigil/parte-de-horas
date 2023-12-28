d4c64e5904ec309d9b854e54e1aa3db2510f1c5c
DarthSigil <servantofchristvigil@mail.com>
2023-12-27 22:02:54 -0900

    documented (w/comments) dafuq going on in input-event-util.js

--- src/utils/input-event-util.js
+++ src/utils/input-event-util.js
@@ -1,27 +1,41 @@
-// need to document how worky
-// also research splicity dicity
-// https://stackoverflow.com/questions/15361189/how-to-select-all-other-values-in-an-array-except-the-ith-element#15361261
-// https://stackoverflow.com/questions/59264119/how-do-i-interpolate-the-value-in-certain-data-point-by-array-of-known-points
-// no missing edge cases found yet but keep peepin game yo
+// comments w/ " :-) -> " are for Testing & Experimenting, 4 bugs & changes, if needed
+
+// live user input event handler
 function interceptInputEvent(input, newMaxLen) {
-    input = input.replace(/\D/g, ""); // gonna have to rework this to allow ":"
-    // or think where/how else interpolate when answer ready
-    if (Number(input[0]) < 1) input = input.slice(1, input.length);
+
+    // remove all non number chars
+    input = input.replace(/\D/g, ""); 
+    
+    // prvnt 0 as 1st dgt
+    if (Number(input[0]) < 1) input = input.slice(1, input.length); // :-) ->  remove "1" frm "10:00" & C ? happns
+
+    // 1st num cn't B > 1 !(23:44) in 4-digit-frmt; newMaxLen must B 3
     if (Number(input[0]) > 1) newMaxLen = 3;
+
+    // 2nd num cn't B > 2 !(13:45) in 4-digit-frmt; newMaxLen must B 3
     if (Number(input[1]) > 2 && newMaxLen === 4) newMaxLen = 3;
-    if (Number(input[1]) > 5 && (newMaxLen === 3 || newMaxLen === 4)) input = input.slice(0, 1);
+
+    // 2nd num cn't B > 5 !(16:45, 2:65) in either digit-frmt
+    if (Number(input[1]) > 5 && (newMaxLen === 3 || newMaxLen === 4)) input = input.slice(0, 1); // :-) -> look 4 edge cases
+
+    // 3rd num cn't B > 5 !(12:73) in 4-digit-frmt; newMaxLen must B 3
     if (Number(input[2]) > 5 && newMaxLen === 4) newMaxLen = 3;
+
+    // disallow input 2 B > newMaxLen
     if (input.length > newMaxLen) input = input.slice(0, newMaxLen);
-    if (input.length === 3) {
+    
+    // if (input.length === 3)
         // interpolate ":" between index 0 & 1
-    } 
-    else if (input.length === 4) {
+    // else if (input.length === 4)
         // interpolate ":" between index 1 & 2
-    }
+    
     return [input, newMaxLen];
 }
 
 module.exports = { interceptInputEvent };
 
-// "abcd12:30".replace(/[^0-9]+/g, "");
-// returns "1230"
+// gonna have to rework this to allow ":"
+// or think where/how else interpolate when answer ready
+
+// https://stackoverflow.com/questions/15361189/how-to-select-all-other-values-in-an-array-except-the-ith-element#15361261
+// https://stackoverflow.com/questions/59264119/how-do-i-interpolate-the-value-in-certain-data-point-by-array-of-known-points
